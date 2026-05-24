bdb774e99445a1805d081255a2a2b302f20bd914
James Johnson <jjohnsonjj1251@gmail.com>
2026-05-24 19:34:11 -0400

    Split out allocation logic

---
+++ src/alloc.rs
@@ -0,0 +1,57 @@
+use std::alloc::{GlobalAlloc, Layout, System};
+use std::ptr::NonNull;
+
+/// Allocate a block of memory to hold `num_bits` bits.
+///
+/// Returns a pointer to the allocated memory, or panics if the allocation fails.
+///
+/// Backing memory is a number of `u64` values allocated on the heap. `num_bits` is rounded up to the nearest multiple of 64 bits, so `num` is the number of `u64` values needed.
+///
+/// # Panics
+/// Panics if `num_bits` is less than 64. This allocator is specifically for the APInt use case which will not allocate for anything less than that.
+///
+/// Also panics if the allocation fails.
+pub fn alloc_bits(num_bits: u32) -> NonNull<u64> {
+    assert!(num_bits > 64, "Allocating a size that should be interned");
+    let num = num_bits.div_ceil(64);
+    let num = num as usize;
+    // SAFETY: Requirements of this function are that align is not zero, align is a power of two, and size rounded up to the nearest multiple of align does not overflow isize.
+    // The alignment is calculated by std::mem::align_of::<u64>() which should just be 8. That is non-zero and a power of two. The size will be some multiple
+    // of the size of u64 which is also 8 bytes. We limit to allocating at most u32 u64s. That means the largest value will be u32::MAX * 8 bytes which will not
+    // overflow an isize on a 64 bit system.
+    let layout = unsafe {
+        Layout::from_size_align_unchecked(
+            num * std::mem::size_of::<u64>(),
+            std::mem::align_of::<u64>(),
+        )
+    };
+    // SAFETY: This method requires that size must be non-zero. We assert that at the entry to the function so we know that to be the case.
+    let ptr = unsafe { System.alloc_zeroed(layout) };
+    if ptr.is_null() {
+        std::alloc::handle_alloc_error(layout);
+    }
+    // SAFETY: Checked for the pointer being null above so we know it's valid here.
+    unsafe { NonNull::new_unchecked(ptr.cast()) }
+}
+
+/// Free the memory used by a value with `num_bits` bits.
+///
+/// # Panics
+/// Panics if `num_bits` is less than 64. This allocator is specifically for the APInt use case which will not allocate for anything less than that.
+pub fn free_bits(ptr: NonNull<u64>, num_bits: u32) {
+    assert!(
+        num_bits > 64,
+        "Freeing a size that should have been interned"
+    );
+    let num = num_bits.div_ceil(64);
+    let num = num as usize;
+    // SAFETY: Requirements of this function are that align is not zero, align is a power of two, and size rounded up to the nearest multiple of align does not overflow isize.
+    let layout = unsafe {
+        Layout::from_size_align_unchecked(
+            num * std::mem::size_of::<u64>(),
+            std::mem::align_of::<u64>(),
+        )
+    };
+    // SAFETY: This method requires that size must be non-zero. We assert that at the entry to the function so we know that to be the case.
+    unsafe { System.dealloc(ptr.as_ptr().cast(), layout) };
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::alloc::{GlobalAlloc, Layout, System};
+mod alloc;
 use std::ptr::NonNull;
 
 pub struct Value {
@@ -14,11 +14,59 @@ pub struct Value {
 }
 
 impl Value {
+    #[inline]
     pub fn new_u8(val: u8) -> Self {
         Self {
             num_bits: 8,
             value: Backing { val: val as u64 },
         }
+    }
+
+    pub fn parse_from_words(data: &[u64], num_bits: u32) -> Self {
+        let mut value = if num_bits <= 64 {
+            Self {
+                num_bits,
+                value: Backing { val: data[0] },
+            }
+        } else {
+            let num_words = num_bits.div_ceil(64);
+            assert!(data.len() >= num_words as usize, "Not enough data to parse");
+            let ptr = crate::alloc::alloc_bits(num_bits);
+            // SAFETY: Requirements: source and destination must be valid for reads/writes of `num_words` u64s and they must be properly aligned.
+            // A softer requirement is that they must not overlap in a way that could invalidate anything.
+            // We know that both are valid for `num_words` reads and writes because we check the size of source and allocate that many for the
+            // destination. They must be aligned properly because one came from a slice which guarantees that and our allocator guarantees that
+            // the returned pointer is properly aligned for a u64. Lastly, we know there is no overlap since the destination is a new allocation.
+            unsafe { std::ptr::copy(data.as_ptr(), ptr.as_ptr(), num_words as usize) };
+            Self {
+                num_bits,
+                value: Backing { ptr },
+            }
+        };
+        value.clear_unused_bits();
+        value
+    }
+
+    /// Clear any high bits that are not used.
+    fn clear_unused_bits(&mut self) {
+        let zero_bits = 64 - (self.num_bits % 64);
+        let mask = u64::MAX >> zero_bits;
+        if self.interned() {
+            unsafe {
+                self.value.val &= mask;
+            }
+        } else {
+            unsafe {
+                let slice = self.as_slice_mut();
+                slice[slice.len() - 1] &= mask;
+            }
+        }
+    }
+
+    /// Get the size of the represented value in bits.
+    #[inline]
+    pub fn size(&self) -> u32 {
+        self.num_bits
     }
 
     /// Check if the value is stored directly in the associated union or not.
@@ -53,6 +101,35 @@ impl Value {
         // SAFETY: This is a pointer to `self.byte_size()` u64s on the heap. This slice is valid.
         unsafe { std::slice::from_raw_parts(ptr.as_ptr(), num_bytes as usize) }
     }
+
+    /// Get a mutable slice to the bytes on the heap.
+    ///
+    /// # Safety
+    /// Only safe if the backing data is actually stored on the heap. You can check that by calling [`Self::interned`].
+    /// You may only call this method if that function would return false.
+    pub unsafe fn as_slice_mut(&mut self) -> &mut [u64] {
+        // SAFETY: Safety contract of this function requires that the struct actually contains a pointer. So this is
+        // safe as long as the function's safety contract is satisfied.
+        let ptr = unsafe { self.value.ptr };
+        let num_bytes = self.byte_size();
+        // SAFETY: This is a pointer to `self.byte_size()` u64s on the heap. This slice is valid.
+        unsafe { std::slice::from_raw_parts_mut(ptr.as_ptr(), num_bytes as usize) }
+    }
+}
+
+impl Clone for Value {
+    fn clone(&self) -> Self {
+        if self.interned() {
+            Self {
+                num_bits: self.num_bits,
+                value: Backing {
+                    val: unsafe { self.value.val },
+                },
+            }
+        } else {
+            todo!()
+        }
+    }
 }
 
 impl Drop for Value {
@@ -60,11 +137,7 @@ impl Drop for Value {
         if !self.interned() {
             // SAFETY: We know the union has a pointer because the data is not interned.
             let ptr = unsafe { self.value.ptr };
-            let num_allocated = self.num_words();
-            let layout = Layout::array::<u64>(num_allocated)
-                .expect("Should be able to create layout for something we allocated");
-            // SAFETY: The layout has nonzero size since we don't support nonzero sizes.
-            unsafe { System.dealloc(ptr.as_ptr() as *mut _, layout) };
+            crate::alloc::free_bits(ptr, self.num_bits);
         }
     }
 }
@@ -81,11 +154,8 @@ impl From<&[u64]> for Value {
             let num_bits: u32 = (64usize.saturating_mul(value.len()))
                 .try_into()
                 .expect("Can't represent that many bits");
-            let layout =
-                Layout::array::<u64>(value.len()).expect("Can't create layout for backing memory");
-            // SAFETY: The size can't be zero.
-            let ptr = unsafe { System.alloc(layout) };
-            let ptr = NonNull::new(ptr as *mut u64).expect("Allocation failed");
+
+            let ptr = crate::alloc::alloc_bits(num_bits);
             Self {
                 num_bits,
                 value: Backing { ptr },
@@ -137,3 +207,23 @@ union Backing {
 }
 
 const _: () = assert!(std::mem::size_of::<Backing>() == 8);
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn interns_small_values() {
+        let small = Value::new_u8(128);
+        assert!(small.interned());
+
+        let small_slice = Value::parse_from_words(&[4096], 48);
+        assert!(small_slice.interned());
+    }
+
+    #[test]
+    fn allocates_large_values() {
+        let large = Value::parse_from_words(&[4096, 4096], 96);
+        assert!(!large.interned());
+    }
+}
